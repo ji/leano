2e071af57f74bdfe8c8d64ac497efbf12e7f9333
Yovoslav Ivanov <yovoslav.ivanov@gmail.com>
2011-12-23 20:26:59 +0100

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Leano::Application.config.secret_token = 'e2a710f85c62017a6973347fe96a13ff99e9710a43a0e3247e635eeb72626e309134988880de7762ede35b514e5f6b6fc0b5161055f83843c9ca2be38411893c'
