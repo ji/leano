7f4475a3202ff5ce5a7ac339d63b6fe76c2bdd3c
Oliver Letterer <oliver.letterer@googlemail.com>
2012-01-07 13:41:51 +0100

    Application controller authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,23 @@
 class ApplicationController < ActionController::Base
+  before_filter :fetch_logged_user
+  
   protect_from_forgery
+  
+protected
+  
+  def fetch_logged_user
+    unless session[:user_id].nil?
+      @logged_user = User.find(session[:user_id])
+    end
+    rescue ActiveRecord::RecordNotFound
+  end
+  
+  
+  def authenticate
+      unless @logged_user
+        # unauthorized access
+        redirect_to login_url, notice: "You need to to login to view this content."
+        return false
+      end
+    end
 end
