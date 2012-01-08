0eb4c7f433e3919c7e87c6785299ceb7c2d73666
Oliver Letterer <oliver.letterer@googlemail.com>
2012-01-08 12:21:41 +0100

    Columns now auto destroy associated user_stories.

--- app/models/column.rb
+++ app/models/column.rb
@@ -1,4 +1,8 @@
 class Column < ActiveRecord::Base
   belongs_to :kanban
   has_many :user_stories
+  
+  before_destroy { |column|
+    column.user_stories.destroy_all
+  }
 end
--- test/unit/column_test.rb
+++ test/unit/column_test.rb
@@ -1,7 +1,14 @@
 require 'test_helper'
 
 class ColumnTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should auto destroy associated objects" do
+    column = Column.new(name: "Backlog")
+    column.user_stories<< UserStory.new
+    
+    assert column.save, "Column should save #{column.errors.full_messages}"
+    
+    column_id = column.id
+    column.destroy
+    assert UserStory.count(:all, conditions: {column_id: column_id}) == 0, "Destroying a column should auto destroy associated user stories"
+  end
 end
