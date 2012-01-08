c6624da715498567b9f4e14de523e4ecad4fb737
Oliver Letterer <oliver.letterer@googlemail.com>
2012-01-08 10:59:22 +0100

    User model now validates email with a regex.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
   validates :name, presence: true, uniqueness: true
-  validates :email, presence: true, uniqueness: true
+  validates :email, presence: true, uniqueness: true, format: {with: /\A[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]+\z/, message: "E-Mail invalid"}
   has_secure_password
   
   has_many :contributors
