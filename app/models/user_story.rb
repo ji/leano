a9cf3d66b28d4fa9617930afe0caeedf1562e3ae
Oliver Letterer <oliver.letterer@googlemail.com>
2012-01-08 12:30:04 +0100

    User stories now auto destroy associated objects.

--- app/models/user_story.rb
+++ app/models/user_story.rb
@@ -3,4 +3,9 @@ class UserStory < ActiveRecord::Base
   belongs_to :column
   has_many :user_story_tags
   has_many :user_story_tasks
+  
+  before_destroy { |user_story|
+    user_story.user_story_tags.destroy_all
+    user_story.user_story_tasks.destroy_all
+  }
 end
--- test/unit/user_story_test.rb
+++ test/unit/user_story_test.rb
@@ -1,7 +1,16 @@
 require 'test_helper'
 
 class UserStoryTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should auto destroy associated objects" do
+    user_story = UserStory.new
+    user_story.user_story_tags<< UserStoryTag.new
+    user_story.user_story_tasks<< UserStoryTask.new
+    
+    assert user_story.save, "User story should save #{user_story.errors.full_messages}"
+    
+    user_story_id = user_story.id
+    user_story.destroy
+    assert UserStoryTag.count(:all, conditions: {user_story_id: user_story_id}) == 0, "Destroying a user story should auto destroy associated tags"
+    assert UserStoryTask.count(:all, conditions: {user_story_id: user_story_id}) == 0, "Destroying a user story should auto destroy associated tasks"
+  end
 end
