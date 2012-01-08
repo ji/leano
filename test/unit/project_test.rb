fa513defeb5c9c7641462ad02afaa75c49f2f393
Oliver Letterer <oliver.letterer@googlemail.com>
2012-01-08 10:35:24 +0100

    Updates Project unit tests to reflect new default Kanban.

--- test/unit/project_test.rb
+++ test/unit/project_test.rb
@@ -7,6 +7,14 @@ class ProjectTest < ActiveSupport::TestCase
   test "should save project" do
     project = Project.new
     project.title = "Test Project"
+    
     assert project.save, "Project title is not set."
+  end
+  
+  test "New Projects Kanban" do
+    project = Project.new(title: "Test Project")
+    
+    assert_not_nil project.kanban, "Projects should come with a Kanban by default"
+    assert_equal project.title, project.kanban.title, "Title of associated Kanban should be equal to owning Projects one"
   end
 end
